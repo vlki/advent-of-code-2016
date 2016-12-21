2e3f1c1c2c8a468b23e8529f077e7d4d3f248b5b
Jan Vlcek <vlki@vlki.cz>
2016-12-21 11:06:57 +0100

    Day 16, part 1

---
+++ 16a/README.md
@@ -0,0 +1,11 @@
+# Advent of Code day 16, part 1
+
+Download and build with dependencies
+```
+stack build
+```
+
+... and run:
+```
+stack exec -- runghc src/Main.hs
+```
---
+++ 16a/src/Main.hs
@@ -0,0 +1,36 @@
+module Main where
+
+import Data.List
+import qualified Debug.Trace as Trace
+
+flipZeroAndOne :: Char -> Char
+flipZeroAndOne '0' = '1'
+flipZeroAndOne '1' = '0'
+
+flipZeroesAndOnes :: String -> String
+flipZeroesAndOnes s = map flipZeroAndOne s
+
+expandUsingDragonCurve :: Int -> String -> String
+expandUsingDragonCurve targetLength s
+    | length s >= targetLength = take targetLength s
+    | otherwise = expandUsingDragonCurve targetLength (s ++ "0" ++ reverse (flipZeroesAndOnes s))
+
+oddEvenSplit :: [a] -> ([a], [a])
+oddEvenSplit [] = ([], [])
+oddEvenSplit (x:y:xs) = (x:xp, y:yp) where (xp, yp) = oddEvenSplit xs
+
+checksumPair :: (Char, Char) -> Char
+checksumPair ('0', '0') = '1'
+checksumPair ('1', '1') = '1'
+checksumPair ('0', '1') = '0'
+checksumPair ('1', '0') = '0'
+
+checksum :: String -> String
+checksum s
+    | odd (length s) = s
+    | otherwise = checksum (map checksumPair (zip odds evens))
+    where (odds, evens) = oddEvenSplit s
+
+main :: IO ()
+main = do
+    putStrLn $ checksum $ expandUsingDragonCurve 272 "01111001100111011"
